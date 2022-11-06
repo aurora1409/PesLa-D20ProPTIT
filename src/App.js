4704f5078a2b0c7fac49c97cc5ca53fb40346c36
aurora1409 <aurora1409>
2022-11-06 17:12:22 +0700

    update Register

--- src/App.js
+++ src/App.js
@@ -1,69 +1,69 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { setToken } from "./Store"; // import từ file store/index.js
-import { Login } from "./Axios";
-
+import Axios, { Login } from "./Axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Register from "./Components/Registers/Register";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   // muốn gọi setState thì dùng useDispatch
   // muốn gọi state thì dùng useSelect
 
   // ví dụ mình gọi user ra log thử
-  const user = useSelector(state => state.user);
+  const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
 
   // console.log(user.token); // đây là lấy token
   // console.log(user.name); // dây là lấy tên
   // console.log(user.product);
-
+  <Register />;
   useEffect(() => {
     // cái setToken này  setToken: (user, actions) => { }
     // nhưng mà chỉ cần truyền 1 tham số vào là đủ
-
     // dispatch(setToken("Token: Đưc Anh"));
-
     // gọi PromiseLogin
-    Login("username123", "password456")
-      .then(response => {
-        console.log(response.data);
-        // response.data nếu mà đăng nhập thành công sẽ có cả token
-        // lúc đấy sẽ gọi setToken(token) -> user.token trong store
-        // user.token -> dùng cho API
-        // Test chức năng add order.
-        // Test chức năng get order by current user.
-      })
-      .catch(error => {
-        console.log(error);
-      });
-
+    // Login("username123", "password456")
+    //   .then((response) => {
+    //     console.log(response.data);
+    //     // response.data nếu mà đăng nhập thành công sẽ có cả token
+    //     // lúc đấy sẽ gọi setToken(token) -> user.token trong store
+    //     // user.token -> dùng cho API
+    //     // Test chức năng add order.
+    //     // Test chức năng get order by current user.
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
     // Key, Value
     // Key: gõ luôn trong file
     // https://viblo.asia/p/local-storage-voi-javascript-djeZ1Gjj5Wz
-
     // Lưu token --> luu vào trong localStorage
     // if local.get(token) != null --> đã từng đăng nhập -> nhảy vào luôn ko cần đăng nhập
-    localStorage.setItem(
-      "Petsla",
-      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjY3NTc3NDMxLCJpYXQiOjE2Njc0OTEwMzEsImp0aSI6IjIwOTMxOTI0ODFjNTQ0ZmRiNTQxOTU1ZDYxZDc5YTU4IiwidXNlcl9pZCI6MTB9.tSFk6YGEf0N-H7rSOh4DoRagCbCe6Jk5A2NwXrctL-s"
-    );
+    // localStorage.setItem(
+    //   "Petsla",
+    //   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjY3NTc3NDMxLCJpYXQiOjE2Njc0OTEwMzEsImp0aSI6IjIwOTMxOTI0ODFjNTQ0ZmRiNTQxOTU1ZDYxZDc5YTU4IiwidXNlcl9pZCI6MTB9.tSFk6YGEf0N-H7rSOh4DoRagCbCe6Jk5A2NwXrctL-s"
+    // );
     // Tắt bỏ máy tính sau mở lại vẫn lưu
   });
 
-  console.log(localStorage.getItem("Petsla"));
+  //console.log(localStorage.getItem("Petsla"));
 
   return (
     <div className="App">
+      <BrowserRouter>
+        {/* //demo Toast thông báo 
       <button
         onClick={() => {
           toast("Wow so easy!");
-        }}>
+        }}
+      >
         Notify!
-      </button>
-      {/* <ToastContainer
+      </button> */}
+
+        {/* <ToastContainer
         position="bottom-center"
         autoClose={5000}
         hideProgressBar={false}
@@ -75,20 +75,12 @@ function App() {
         pauseOnHover
         theme="colored"
       /> */}
-      <ToastContainer />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+        {/* <ToastContainer /> */}
+        <Register />
+        <Routes>
+          <Route path="/register" component={Register} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Registers/Register.jsx
@@ -0,0 +1,160 @@
+import React, { useEffect } from "react";
+import "./Register.css";
+import index from "../../Axios/index";
+import Axios from "../../Axios/index";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { Form, Link } from "react-router-dom";
+import { addNewUser, setFirstName, setLastName, setEmail, setUsername, setPassword } from "../../Store/index";
+import { useState } from "react";
+
+// export default khong can dau ngoac {}, ten gi cung duoc
+// export bthg can dau {}
+
+const Register = () => {
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const [firstName,setFirstName] = useState('')
+  const[lastName,setLastName]= useState('')
+  const[email,setEmail]= useState('')
+  const[username,setUsername]= useState('')
+  const[password,setPassword]= useState('')
+  // console.log(user);
+  //const [firstname, setFirstname] = useState('');
+  const handleClickRegister = () => {
+    Axios("register/", "POST", {
+      first_name: firstName,
+      last_name: lastName,
+      email,
+      username,
+      password
+    })
+      .then((res) => {
+        console.log(res.data);
+        // console.log("hi");
+        dispatch(addNewUser(res.data));
+        localStorage.setItem("token", res.data.token.toString());
+        // console.log(localStorage.getItem("token"));
+        console.log(user);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  // const changeInput = data => {
+  //   dispatch(setFirstName(data.target.value))
+  // }
+  // useEffect(() => {
+  //   //   console.log("yes")
+  //   let newUser = {
+  //     first_name: "12dymxn",
+  //     last_name: "m12mxi",
+  //     email: "x@",
+  //     username: "dx1ud1e",
+  //     password: "dxfum11",
+  //   };
+
+  //   Axios("register/", "POST", newUser)
+  //     .then((res) => {
+  //       // console.log(res);
+  //       // console.log("hi");
+  //       dispatch(addNewUser(res.data));
+  //       localStorage.setItem("token", res.data.token.toString());
+  //       // console.log(localStorage.getItem("token"));
+  //       // console.log(user);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
+  //console.log(user)
+
+  return (
+    <>
+      <div className="regisWrap">
+        <div className="registerWrap">
+          <div className="regisHeader">
+            <span className="registerTitle">Register</span>
+            <button
+              type="button"
+              className="btn-close"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="registerMain">
+            <input
+              type="text"
+              className="inputRegister"
+              id="firstName"
+              placeholder="FirstName"
+              onChange={e => {
+                setFirstName(e.target.value)
+                //console.log(firstName)
+              }}
+            />
+            <input
+              type="text"
+              className="inputRegister"
+              id="lastName"
+              placeholder="LastName"
+              //onChange={event => }
+            />
+            <input
+              type="email"
+              className="inputRegister"
+              id="email"
+              placeholder="Email"
+              onChange={e => {
+                setLastName(e.target.value)
+                //console.log(firstName)
+              }}
+            />
+            <input
+              type="text"
+              className="inputRegister"
+              id="username"
+              placeholder="UserName"
+              onChange={e => {
+                setUsername(e.target.value)
+                //console.log(firstName)
+              }}
+            />
+            <input
+              type="password"
+              className="inputRegister"
+              id="password"
+              placeholder="Password"
+              onChange={e => {
+                setPassword(e.target.value)
+                //console.log(firstName)
+              }}
+            />
+          </div>
+          <button className="registerBtn" id="registerBtnMain" onClick={handleClickRegister}>
+            Register
+          </button>
+          {/* (event) => {
+            axios(/re, {
+              firstname,
+              ///.
+            }) */}
+          <div className="otherMethodRegister">
+            <button className="registerBtn" id="registerGGBtn" >
+              Register with Google
+            </button>
+            <button className="registerBtn" id="registerFbBtn">
+              Register with Facebook
+            </button>
+          </div>
+          <div className="registerFooter">
+            <Link to="/">Already have an account? Login</Link>
+            {/* <a href="">Already have an account? Login</a> */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Register;
