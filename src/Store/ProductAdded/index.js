e55d31d6553af882592255605ad1b2a00cd51871
DucAnh <ducanh2k2@gmail.com>
2022-11-15 02:20:01 +0700

    build cart

--- src/Store/ProductAdded/index.js
+++ src/Store/ProductAdded/index.js
@@ -2,17 +2,66 @@ import { createSlice } from "@reduxjs/toolkit";
 const ProductAdded = createSlice({
   name: "productadded",
   initialState: {
+    /* [[item, count]] */
     productList: [],
   },
   reducers: {
     addProduct: (state, action) => {
-      state.productList.push(action.payload);
+      try {
+        const item = action.payload;
+        const idx = state.productList.findIndex(
+          ([key, count], idx) => key.id === item.id
+        );
+        if (idx === -1) {
+          state.productList.push([item, 1]);
+        } else {
+          state.productList[idx][1] += 1;
+        }
+      } catch (err) {
+        console.log(err);
+      }
     },
-    increaseProduct: (state, action) => {},
-    decreaseProduct: (state, action) => {},
-    removeProduct: (state, action) => {},
+    increaseProduct: (state, action) => {
+      try {
+        const id = action.payload;
+        const idx = state.productList.findIndex(
+          ([key, count], idx) => key.id === id * 1
+        );
+        if (idx !== -1) {
+          state.productList[idx][1] += 1;
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    decreaseProduct: (state, action) => {
+      try {
+        const id = action.payload;
+        const idx = state.productList.findIndex(
+          ([key, count], idx) => key.id === id * 1
+        );
+        if (idx !== -1) {
+          state.productList[idx][1] -= 1;
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    removeProduct: (state, action) => {
+      try {
+        const id = action.payload;
+        const idx = state.productList.findIndex(
+          ([key, count], idx) => key.id === id * 1
+        );
+        if (idx !== -1) {
+          state.productList.splice(idx, 1);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    },
     clearProduct: state => {
-      state.productList.clear();
+      state.productList = [];
     },
   },
 });
